bef3de56a6586f6cf6e594175b26298ee69df1ea
hppeng <hppeng.wynn@gmail.com>
2021-08-07 23:15:08 -0700

    Fix font path

--- draw.py
+++ draw.py
@@ -4,11 +4,11 @@ import math
 #NORMAL_FONT = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", 16)
 #BOLD_FONT = ImageFont.truetype("Pillow/Tests/fonts/FreeMonoBold.ttf", 16)
 NORMAL_FONT_SIZE = 16
-NORMAL_FONT = ImageFont.truetype("Pillow/Tests/fonts/FreeSans.ttf", NORMAL_FONT_SIZE)
-BOLD_FONT = ImageFont.truetype("Pillow/Tests/fonts/FreeSansBold.ttf", NORMAL_FONT_SIZE)
+NORMAL_FONT = ImageFont.truetype("./fonts/FreeSans.ttf", NORMAL_FONT_SIZE)
+BOLD_FONT = ImageFont.truetype("./fonts/FreeSansBold.ttf", NORMAL_FONT_SIZE)
 LARGE_FONT_SIZE = 40
-LARGE_FONT = ImageFont.truetype("Pillow/Tests/fonts/FreeSans.ttf", LARGE_FONT_SIZE)
-LARGE_BOLD_FONT = ImageFont.truetype("Pillow/Tests/fonts/FreeSansBold.ttf", LARGE_FONT_SIZE)
+LARGE_FONT = ImageFont.truetype("./fonts/FreeSans.ttf", LARGE_FONT_SIZE)
+LARGE_BOLD_FONT = ImageFont.truetype("./fonts/FreeSansBold.ttf", LARGE_FONT_SIZE)
 
 def break_text(text: str, draw: ImageDraw, font: ImageFont, max_width: float):
     """
