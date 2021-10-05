abcadef8ac76c45cc595f1df5c73009ef5217bc5
hppeng <hppeng>
2021-10-04 23:48:55 -0700

    Fix github init (no more resetting?)

--- github_init.sh
+++ github_init.sh
@@ -1,9 +1,9 @@
 GUILD_ID="$1"
-rm -rf atlas-games_store; git clone https://hppeng-wynn:$GITHUB@github.com/hppeng-wynn/atlas-games_store.git;
+#rm -rf atlas-games_store; git clone https://hppeng-wynn:$GITHUB@github.com/hppeng-wynn/atlas-games_store.git;
 cd atlas-games_store
 git config --global user.name hppeng
 git config --global user.email hppeng
 git branch $GUILD_ID
 git checkout $GUILD_ID
-git pull origin $GUILD_ID
+git pull origin $GUILD_ID --allow-unrelated-histories
 git push --set-upstream origin $GUILD_ID --force
