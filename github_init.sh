42bf3e8509b08838b04d82fed49575c4d04bdf6b
hppeng <hppeng>
2021-10-04 23:57:26 -0700

    Add back the line that... clones the repo......

--- github_init.sh
+++ github_init.sh
@@ -1,5 +1,5 @@
 GUILD_ID="$1"
-#rm -rf atlas-games_store; git clone https://hppeng-wynn:$GITHUB@github.com/hppeng-wynn/atlas-games_store.git;
+rm -rf atlas-games_store; git clone https://hppeng-wynn:$GITHUB@github.com/hppeng-wynn/atlas-games_store.git;
 cd atlas-games_store
 git config --global user.name hppeng
 git config --global user.email hppeng
